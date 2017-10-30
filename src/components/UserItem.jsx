8bedb665eea33e6e461b4ac449ee53d825c9fe91
Louise Camlin <lacamlin@gmail.com>
2017-10-30 16:31:53 +0000

    render star rating system that writes to db when clicked

---
+++ src/components/UserItem.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import * as firebase from 'firebase';
+import ReactStars from 'react-stars'
+
+class UserItem extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      apps: {},
+    }
+    this.getApps()
+  }
+
+
+  getApps(){
+    const appsRef = firebase.database().ref(`accounts/${this.props.account}`);
+    appsRef.on('value', (snapshot) => {
+    this.setState({apps: snapshot.val().apps})
+
+    })
+  }
+
+
+ setAppRating(app, rating){
+   const appsRef = firebase.database().ref(`accounts/${this.props.account}/apps/${app}`);
+   appsRef.update({rating: rating})
+ }
+
+ handleRatingChange(app, rating){
+   this.setAppRating(app, rating)
+ }
+
+
+
+  mapApps(){
+    const apps = Object.keys(this.state.apps).map((app, index) => {
+      console.log("mapped app",this.state.apps[app]);
+      return(
+        <li key={index}>{this.state.apps[app].title} <ReactStars id='rating'
+          name={app}
+          count={5}
+          onChange={(rating) => {
+            this.handleRatingChange(app, rating)
+          }}
+          value={this.state.apps[app].rating}
+          size={24}
+         />
+        </li>
+
+        )
+      })
+    return apps
+  }
+
+
+  render(){
+    const apps = this.mapApps()
+
+    return(
+      <div id='app-item'>
+        <ul>{apps}</ul>
+      </div>
+    )
+  }
+
+}
+
+export default UserItem
